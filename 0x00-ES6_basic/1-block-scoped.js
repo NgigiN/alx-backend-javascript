d8999942b680e324e3fe8459030edc43ddcabb4d
NgigiN <ngigi.nyongo@gmail.com>
2024-04-08 23:25:31 +0300

    block scoping variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,12 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
+  (function() {
+    if (trueOrFalse) {
+      task = true;
+      task2 = false;
+    }
+  })();
 
   return [task, task2];
 }
