87cc455d0682e740bacc566a778a02fb95de3603
NgigiN <ngigi.nyongo@gmail.com>
2024-04-10 17:38:13 +0300

    catch using promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise
-      .then(response => {
+      .then((response) => {
         console.log('Got a response from the API');
         resolve({ status: 200, body: 'Success' });
       })
-      .catch(error => {
-        console.error(error);
+      .catch((error) => {
+        console.error('Error from the API:', error);
         reject(new Error());
       });
   });
