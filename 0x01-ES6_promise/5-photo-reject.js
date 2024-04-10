db531123c01cfe1766674b196f1d342b561a7669
NgigiN <ngigi.nyongo@gmail.com>
2024-04-10 18:37:18 +0300

    returns the correct reject error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,9 @@
-export default function uploadPhoto(filename) {
+function uploadPhoto(filename) {
   return Promise.reject(
     new Error(
       `${filename} cannot be processed`,
     ),
   );
 }
+
+export default uploadPhoto;
