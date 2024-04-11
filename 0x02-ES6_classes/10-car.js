4751929d4cdf2d3612f88d4f217056bd88e28dda
NgigiN <ngigi.nyongo@gmail.com>
2024-04-11 17:33:05 +0300

    adding the appropriate setters and getters

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,6 +5,36 @@ export default class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(brand) {
+    if (typeof (brand) === 'string') {
+      this._brand = brand;
+    }
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(motor) {
+    if (typeof (motor) === 'string') {
+      this._motor = motor;
+    }
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(color) {
+    if (typeof (color) === 'string') {
+      this._color = color;
+    }
+  }
+
   cloneCar() {
     const clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
     return clone;
