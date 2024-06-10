3682987386292a9add4fcdae51274fa17d49d24d
NgigiN <ngigi.nyongo@gmail.com>
2024-06-10 16:08:04 +0300

    Using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
 // This file takes the name of a user via cmd
 // outputs the name and displays a message as it terminates
-
-const readline = require('readline');
-
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-r1.on('line', (name) => {
-  name = name.trim();
-  console.log(`Your name is: ${name}`);
-});
-r1.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
