6f3afaf5bdf7fd9ecd7313fbfc6847252aa7f5d1
NgigiN <ngigi.nyongo@gmail.com>
2024-06-21 23:49:02 +0300

    adjusting tests for the 2 new endpoints

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -15,13 +15,8 @@ app.get('/cart/:id([0-9]+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-app.get('/available_payments', (req, res) => {
-  res.send({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  });
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
