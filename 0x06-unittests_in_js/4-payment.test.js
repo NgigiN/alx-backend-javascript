595051f765951af8bd912e33a00ee6e76fd99be2
NgigiN <ngigi.nyongo@gmail.com>
2024-06-21 14:35:32 +0300

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+// Tests for the SendPaymentRequestToApi function using stubs
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculate method of utils', () => {
+    const spy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    spy.calculateNumber.restore();
+  });
+});
