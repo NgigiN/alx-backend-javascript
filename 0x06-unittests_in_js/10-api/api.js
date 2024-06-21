6a67b58cf9673030dba32f3e551386017b5f3e96
NgigiN <ngigi.nyongo@gmail.com>
2024-06-21 23:57:05 +0300

    adding body parsing middleware to handle json in req properly

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,15 +1,10 @@
 const express = require('express');
-
 const app = express();
+
 const port = 7865;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+app.get('/', (req, res) => res.send('Welcome to the payment system'));
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
@@ -21,13 +16,12 @@ app.get('/available_payments', (_req, res) => {
 
 app.post('/login', (req, res) => {
   let username = '';
-
   if (req.body) {
     username = req.body.userName;
   }
-
   res.send(`Welcome ${username}`);
 });
 
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
 
 module.exports = app;
