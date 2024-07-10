e8be6c3ccfa3c7e15ab84ae06fe4a10d4eca54c5
NgigiN <ngigi.nyongo@gmail.com>
2024-07-10 17:34:05 +0300

    ESlint compliace adjustments

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,51 +1,51 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
   if (!fs.existsSync(dataPath)) {
-    reject(new Error('Cannot load the database'))
-    return
+    reject(new Error('Cannot load the database'));
+    return;
   }
   if (!fs.statSync(dataPath).isFile()) {
-    reject(new Error('Cannot load the database'))
-    return
+    reject(new Error('Cannot load the database'));
+    return;
   }
 
   fs.readFile(dataPath, 'utf-8', (err, data) => {
     if (err) {
-      reject(new Error('Error reading the database'))
-      return
+      reject(new Error('Error reading the database'));
+      return;
     }
 
-    const fileLines = data.trim().split('\n')
-    const studentGroups = {}
-    const dbFieldNames = fileLines[0].split(',')
-    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1)
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
     for (const line of fileLines.slice(1)) {
-      const studentRecord = line.split(',')
-      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1)
-      const field = studentRecord[studentRecord.length - 1]
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
 
       if (!Object.keys(studentGroups).includes(field)) {
-        studentGroups[field] = []
+        studentGroups[field] = [];
       }
 
       const studentEntries = studentPropNames.map((propName, idx) => [
         propName,
         studentPropValues[idx]
-      ])
-      studentGroups[field].push(Object.fromEntries(studentEntries))
+      ]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
     }
 
     // Convert to object of arrays of first names
-    const result = {}
+    const result = {};
     for (const [field, students] of Object.entries(studentGroups)) {
-      result[field] = students.map((student) => student.firstname)
+      result[field] = students.map((student) => student.firstname);
     }
 
-    resolve(result)
-  })
-})
+    resolve(result);
+  });
+});
 
-export default readDatabase
-module.exports = readDatabase
+export default readDatabase;
+module.exports = readDatabase;
