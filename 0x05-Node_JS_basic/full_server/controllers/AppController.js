45fa5ea73032516048f124a33c662e950cd096c2
NgigiN <ngigi.nyongo@gmail.com>
2024-07-10 17:46:44 +0300

    ESlint compliace adjustments

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
 class Appcontroller {
-  static getHomepage (request, response) {
+  static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
 }
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,7 +3,7 @@ const readDatabase = require('../utils');
 const VALID_MAJORS = ['CS', 'SWE'];
 
 class StudentController {
-  static getAllStudents(request, response) {
+  static getAllStudents (request, response) {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
     readDatabase(dataPath)
@@ -38,7 +38,7 @@ class StudentController {
       });
   }
 
-  static getAllStudentsByMajor(request, response) {
+  static getAllStudentsByMajor (request, response) {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = request.params;
 
